0ea1acd6aa5c2ea65914539ac5efc16186b58410
Suhan <65suhanraj@gmail.com>
2026-09-06 10:23:46 +0530

    feat: enable maintenance mode by default

--- lib/config/maintenance.ts
+++ lib/config/maintenance.ts
@@ -16,7 +16,7 @@
  * ============================================================================
  */
 
-export const MAINTENANCE_MODE: boolean = false;
+export const MAINTENANCE_MODE: boolean = true;
 
 /**
  * Checks if Maintenance Mode is active (either via code constant or env var).
